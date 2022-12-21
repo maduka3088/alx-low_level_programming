c9b39800692420fcacbf8f72b2dc6eff59c99654
maduka3088 <maduka3088@gmail.com>
2022-12-20 17:40:13 -0800

    Task 0

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+ This is a readme file on 0x05-pointers_arrays_strings
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+nclude "main.h"
+
+#include <stdio.h>
+
+void pront_rev(char *s);
+
+void rev_string(char *s);
+
+void puts2(char *str);
+
+void puts_half(char *str);
+
+void print_array(int *a, int n);
+
+char *_strcpy(char *dest, char *src);
+
+
+
+#endif
