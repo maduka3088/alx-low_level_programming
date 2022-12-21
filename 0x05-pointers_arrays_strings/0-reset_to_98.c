26ad1b5d9c0850c585f900d12e31daab38f6f9a5
maduka3088 <maduka3088@gmail.com>
2022-12-20 17:57:04 -0800

    0 Task

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,10 +4,12 @@
  *  swap_int .> given two integers swap the values they are holding
  *   @a: parameter 1
  *   @b: parameter 2
- *   */
+ */
 void swap_int(int *a, int *b)
 {
 	int tmp;
 
-	tmp = *a
-		*b = tmp
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
