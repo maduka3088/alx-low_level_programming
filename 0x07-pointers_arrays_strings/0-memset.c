d94fb033506e186c4cb5367fcf1e8b337391319e
maduka3088 <maduka3088@gmail.com>
2023-01-02 08:02:38 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,71 +1,143 @@
+
+
 #include "main.h"
-#include<stdio.h>
+
+
+
+
+
 
 
 /**
  *
- *  * _memset - Fills the first n bytes of the memory area
+ *  *
  *
- *   *          pointed to by @s with the constant byte @c.
+ *   *  * _memset - Fills the first n bytes of the memory area
  *
- *    * @s: A pointer to the memory area to be filled.
+ *    *
  *
- *     * @c: The character to fill the memory area with.
+ *     *   *          pointed to by @s with the constant byte @c.
  *
- *      * @n: The number of bytes to be filled.
+ *      *
  *
- *       * description _memset: over there
+ *       *    * @s: A pointer to the memory area to be filled.
  *
  *        *
  *
- *         * Return: A pointer to the filled memory area @s.
+ *         *     * @c: The character to fill the memory area with.
  *
- *          * _memset - fills a memory block with a constant byte
+ *          *
  *
- *           * @s: address to memory block
+ *           *      * @n: The number of bytes to be filled.
  *
- *            * @b: char to be used
+ *            *
  *
- *             * @n: number of bytes to be used
+ *             *       * description _memset: over there
  *
  *              *
  *
- *               * Return: pointer to the memory block
+ *               *        *
  *
- *                */
+ *                *
+ *
+ *                 *         * Return: A pointer to the filled memory area @s.
+ *
+ *                  *
+ *
+ *                   *          * _memset - fills a memory block with a constant byte
+ *
+ *                    *
+ *
+ *                     *           * @s: address to memory block
+ *
+ *                      *
+ *
+ *                       *            * @b: char to be used
+ *
+ *                        *
+ *
+ *                         *             * @n: number of bytes to be used
+ *
+ *                          *
+ *
+ *                           *              *
+ *
+ *                            *
+ *
+ *                             *               * Return: pointer to the memory block
+ *
+ *                              *
+ *
+ *                               *                */
+
+
 
 void *_memset(void *s, int c, size_t n)
 
+
+
 {
 
-		unsigned int index;
-
-			unsigned char *memory = s, value = c;
 
 
-
-				for (index = 0; index < n; index++)
-
-							memory[index] = value;
+			unsigned int index;
 
 
 
-					return (memory);
+						unsigned char *memory = s, value = c;
 
-					char *_memset(char *s, char b, unsigned int n)
 
-					{
 
-							while (n)
 
-									{
 
-												s[n - 1] = b;
 
-														n--;
 
-															}
+										for (index = 0; index < n; index++)
 
-								return (s);
 
-					}
+
+																		memory[index] = value;
+
+
+
+
+
+
+
+															return (memory);
+
+
+
+																				char *_memset(char *s, char b, unsigned int n)
+
+
+
+																										{
+
+
+
+																																		while (n)
+
+
+
+																																												{
+
+
+
+																																																									s[n - 1] = b;
+
+
+
+																																																																							n--;
+
+
+
+																																																																																						}
+
+
+
+																																										return (s);
+
+
+
+																																															}
