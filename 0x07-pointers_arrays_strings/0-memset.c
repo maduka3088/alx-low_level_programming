c44c0bac4e3b39aa6123e9c50d4e6b5bf4494601
maduka3088 <maduka3088@gmail.com>
2023-01-03 08:00:23 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,10 +11,12 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	while (n)
-	{
-		s[n - 1] = b;
-		n--;
-	}
+	int i(0);
+		while (n > 0)
+		{
+			s[i] = b;
+			i++;
+			n--;
+		}
 	return (s);
 }
